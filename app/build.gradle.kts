f3a4647855e73fa6a68c5fd231afc0a66619eb6a
planqton <planqton@users.noreply.github.com>
2026-01-24 12:45:24 +0100

    Release v1.0.9
    - versionName: 1.0.9
    - versionCode: 10009

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "at.planqton.directcan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10008
-        versionName = "1.0.8"
+        versionCode = 10009
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
